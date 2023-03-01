f9a6384cd1dfc82077f55d56986f3d7c2264b3f5
adolfoiracheta <84049381+adolfoiracheta@users.noreply.github.com>
2023-03-01 10:05:45 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Array-Mean-of-Numbers
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5puy8m)
---
+++ index.js
@@ -0,0 +1,18 @@
+//make an array with numbers
+//your code should return the mean of those numbers
+
+let array1 = ['4', '11', '88', '953'];
+
+for (let i = 0; i < array1.length; i ++) {
+
+//If statement determines the condition
+//If condition is True inside the If statement run the code
+
+if (array1[i]%2 == 0) {
+  console.log(array1[i] + ' is even' )
+  }
+else {
+  console.log(array1[i] + ' is odd ')
+  }
+}
+
